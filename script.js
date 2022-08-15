a9c3504e0144cea1b68afecc20346ab2068c7fd3
SapphireBlueSkittlez <angelinespo@gmail.com>
2022-08-15 13:13:53 -0400

    Add operate function to perform calculations

--- script.js
+++ script.js
@@ -14,7 +14,29 @@ function divide(a, b) {
     return a / b;
 }
 
+function operate(operator, a, b) {
+    switch(operator) {
+        case '+':
+            return add(a, b);
+            break;
+        case '-':
+            return subtract(a, b);
+            break;
+        case '*':
+            return multiply(a, b);
+            break;
+        case '/':
+            return divide(a, b);
+            break;
+    }
+}
+
 console.log(add(1, 2));
 console.log(subtract(1, 2));
 console.log(multiply(2, 2));
 console.log(divide(4, 2));
+console.log('++++++++');
+console.log(operate('+', 1, 2));
+console.log(operate('-', 1, 2));
+console.log(operate('*', 2, 2));
+console.log(operate('/', 4, 2));
